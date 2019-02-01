da270a3d1b7a93fe83cd93705bfea6e7c488961f
vishalikannan <46967715+vishalikannan@users.noreply.github.com>
2019-02-01 09:33:49 +0530

    Update natural.py

--- natural.py
+++ natural.py
@@ -2,9 +2,12 @@ num=int(input())
 hold=num
 sum=0
 if num<=0:
+  
   print("Positive number:)
 else:
-  while num>0:
+        
+while num>0:
+        
     sum=sum+num
     num=num-1
-    print("Natural number")
+  print("Natural number")
