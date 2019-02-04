91b3da4ba0453772e2ca766e71668f7753af0a89
vishalikannan <46967715+vishalikannan@users.noreply.github.com>
2019-02-04 13:53:25 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -2,9 +2,10 @@
 int main()
 {
 char ch;
+  scanf("%c",&ch);
 if(ch=='a'||ch=='e'||ch='i'||ch=='o'||ch=='u'||ch=='A'||ch='E'||ch=='I'||ch=='O'||ch=='U')
 printf("Vowels");
 else
-printf("Consonent")
+printf("Consonant")
 return 0;
 }
