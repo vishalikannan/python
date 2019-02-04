88dcf61c7f03fca1e946250de2ddd13233544535
vishalikannan <46967715+vishalikannan@users.noreply.github.com>
2019-02-04 14:32:02 +0530

    Update large.c

--- large.c
+++ large.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c;
-scanf("%d%d%d",&a,&b,&c);
-if(a>b&&a>c)
-printf("%d",a);
-else if(b>c)
-printf("%d",b);
+int x,y,z;
+scanf("%d%d%d",&x,&y,&z);
+if(x>y&&x>z)
+printf("%d",x);
+else if(y>z)
+printf("%d",y);
 else
-printf("%d",c);
+printf("%d",z);
 return 0;
 }
