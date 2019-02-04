cb6418a47641af0e65a085cd452584d5c9ef9333
vishalikannan <46967715+vishalikannan@users.noreply.github.com>
2019-02-04 14:06:14 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -5,7 +5,9 @@ char ch;
   scanf("%c",&ch);
 if(ch=='a'||ch=='e'||ch='i'||ch=='o'||ch=='u'||ch=='A'||ch='E'||ch=='I'||ch=='O'||ch=='U')
 printf("Vowels");
+else if((ch>='a' && ch<='z')||(ch>='A' && ch<='Z'))
+printf("Consonant");
 else
-printf("Consonant")
+  printf("invalid")
 return 0;
 }
