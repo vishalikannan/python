d42b1d97cf89f104d8003236449732d198d1c1f2
vishalikannan <46967715+vishalikannan@users.noreply.github.com>
2019-02-04 13:42:21 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+if(ch=='a'||ch=='e'||ch='i'||ch=='o'||ch=='u'||ch=='A'||ch='E'||ch=='I'||ch=='O'||ch=='U')
+printf("Vowels");
+else
+printf("Consonent")
+return 0;
+}
