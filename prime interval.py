f88313b570f28a03bfca3751845967f0f2d64168
vishalikannan <46967715+vishalikannan@users.noreply.github.com>
2019-02-07 21:28:57 +0530

    Create prime interval.py

---
+++ prime interval.py
@@ -0,0 +1,11 @@
+n,k=raw_input().split()
+n=int(n)
+k=int(k)
+for i in range(n+1,k):
+   j=0
+   if i>1:
+      for i in range(2,i):
+        if i%s==0:
+          j=j+1
+   if j<=0:
+      print i,
