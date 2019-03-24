66a7b4035abb3bd93c073571aff0304b1d193893
vishalikannan <46967715+vishalikannan@users.noreply.github.com>
2019-03-24 15:42:59 +0530

    Update hunt3.py

--- hunt3.py
+++ hunt3.py
@@ -3,7 +3,7 @@ import sys,string,math
 n=int(input())
 L=list(map(int,input().split()))
 L2=[]
-for i in range(n);
+for i in range(n):
     if L[i]==i: L2.append(i)
     L3=sorted(L2)
 if len(L3)==0: print(-1)
