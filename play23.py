779797c9495e8b54cf7d3be7a6bbb02098dd46a7
vishalikannan <46967715+vishalikannan@users.noreply.github.com>
2019-03-24 21:31:44 +0530

    Update play23.py

--- play23.py
+++ play23.py
@@ -7,6 +7,6 @@ L1 = [ int(x) for x in input().split()]
 L2 = [ int(x) for x in input().split()]
 L3 = []
 for i in range(0,k) :
-    L1.append(L2[i])
-    L3.append(max(L1))
+     L1.append(L2[i])
+     L3.append(max(L1))
 print(*L3)
