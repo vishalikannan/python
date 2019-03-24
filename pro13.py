bdfcb3d205b9e88ede9cf92d91d494ed5a54cf46
vishalikannan <46967715+vishalikannan@users.noreply.github.com>
2019-03-24 20:38:06 +0530

    Update pro13.py

--- pro13.py
+++ pro13.py
@@ -4,6 +4,6 @@ n,k = input().split()
 n,k = int(n), int(k)
 L = [ int(x) for x in input().split()]
 for i in range(0,k) :
-    a,b = input().split()
-    a,b = int(a), int(b)
-    print(min(L[a-1:b]))
+     a,b = input().split()
+     a,b = int(a), int(b)
+     print(min(L[a-1:b]))
