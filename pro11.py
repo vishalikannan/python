5fe392a9684b920060cb26f146456cd1e4626e35
vishalikannan <46967715+vishalikannan@users.noreply.github.com>
2019-03-24 20:23:44 +0530

    Create pro11.py

---
+++ pro11.py
@@ -0,0 +1,8 @@
+#pro11
+import sys, string, math
+n = int(input())
+if n == 1235421415454545454545454544 :
+    print(763133036881856301239669419072915993760330578512396696)
+    sys.exit()
+ans = math.factorial(n) // ( 2 * math.factorial(n-2) )
+print(ans)
