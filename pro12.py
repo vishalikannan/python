0c5294e5eaef4f5729cc1be4f297e2caae11a1d1
vishalikannan <46967715+vishalikannan@users.noreply.github.com>
2019-03-24 20:29:14 +0530

    Create pro12.py

---
+++ pro12.py
@@ -0,0 +1,9 @@
+#pro12
+import sys, string, math
+n,k = input().split()
+n,k = int(n), int(k)
+L = [ int(x) for x in input().split()]
+for i in range(0,k) :
+    a,b = input().split()
+    a,b = int(a), int(b)
+    print(sum(L[a-1:b]))
