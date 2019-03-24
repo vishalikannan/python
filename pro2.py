b3f635d1bcf0acdd852b5103e82d70ff92801065
vishalikannan <46967715+vishalikannan@users.noreply.github.com>
2019-03-24 17:27:39 +0530

    Update pro2.py

--- pro2.py
+++ pro2.py
@@ -1,18 +1,18 @@
 
 import sys,string,math
-def reduceN(n,k):
-    if k<=0:return n
-    if n==0:return 10
-    p1=reduceN(n//10,k)*10+n%10
-    p2=reduceN(n//10,k-1)
-    if p1<p2:
+def reduceN( n , k ):
+    if k <= 0 : return n
+    if n == 0 : return 10
+    p1 = reduceN(n//10, k)*10 + n%10
+    p2 = reduceN(n//10, k-1)
+    if p1 < p2 :
         return p1
     
     
-    else:
+    else :
         return p2
     
     
-n,k=input().spilt()
-n,k=int(n),int(k)
+n,k = input().split()
+n,k = int(n),int(k)
 print(reduceN(n,k))
