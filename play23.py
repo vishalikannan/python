23f4cb6c6bd24ae07d0c6d0bd2d9bc437e542c12
vishalikannan <46967715+vishalikannan@users.noreply.github.com>
2019-03-24 21:28:21 +0530

    Create play23.py

---
+++ play23.py
@@ -0,0 +1,12 @@
+#play23
+import sys,string, math,itertools
+ 
+n,k = input().split()
+n,k = int(n),int(k)
+L1 = [ int(x) for x in input().split()]
+L2 = [ int(x) for x in input().split()]
+L3 = []
+for i in range(0,k) :
+    L1.append(L2[i])
+    L3.append(max(L1))
+print(*L3)
