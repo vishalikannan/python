fb4299127db6b2b9a33158affef511f2af171075
vishalikannan <46967715+vishalikannan@users.noreply.github.com>
2019-03-24 15:54:51 +0530

    Update hunt4.py

--- hunt4.py
+++ hunt4.py
@@ -1,7 +1,7 @@
 import sys,string,math
-n-int(input())
+n=int(input())
 L=list(map(int,input().split()))
 for i in range(len(L)):
     if L.count(L[i])==1:
-        print([i])
+        print(L[i])
         break
