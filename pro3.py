3b8639f0c14fe8a655051819f05d16638521aa06
vishalikannan <46967715+vishalikannan@users.noreply.github.com>
2019-03-24 17:46:47 +0530

    Create pro3.py

---
+++ pro3.py
@@ -0,0 +1,27 @@
+import sys,string,math
+s1,s2 = input().split()
+n1 = len(s1)
+n2 = len(s2)
+if n2 > n1 :
+    i=0
+    while i<n1 and s1[i] == s2[i] :
+        i += 1
+    print(n2-i)
+elif n2 == n1 ;
+    i=0
+    while i<n2 and s1[i] == s2[i] :
+        i += 1
+    print(n2-i)
+else :
+    i=0
+    while i<n2 and s1[i] == s2[i] :
+        i += 1
+    s31 = s1[i:]
+    s32 = s2[i:]
+    L = list(s31)
+    k = 0
+    for c in s32 :
+        if c in L :
+            k +=1
+            L.remove(c)
+    print(n1-i-k)
